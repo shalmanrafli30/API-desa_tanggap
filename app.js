1ab0f8b792582bf7fcd323ef34505084fbad40ab
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-05 01:06:58 +0700

    middleware const added

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const morgan = require('morgan');
-
+const middlewareLogReq = require('./middleware/logs');
 const userRoutes = require('./routes/userRoutes');
 
 app.use(morgan('dev'))
 
+app.use(middlewareLogReq);
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found!');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
