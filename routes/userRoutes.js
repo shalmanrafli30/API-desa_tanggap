7b7cc9024149a62e57a355086952cf472796dc8e
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-05 04:26:57 +0700

    revision routes for update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,6 @@ const userController = require('../controllers/userControllers');
 router.get('/', userController.getAllUsers);
 router.get('/:id_user', userController.getUserById);
 router.post('/registrasi', userController.addUser);
-router.patch('/edit/:id_user', userController.updateUser);
+router.patch('/:id_user', userController.updateUser);
 
 module.exports = router;
