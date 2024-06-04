d1224ff3716bde1a1300e8e038aa8b69ef14960e
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-05 00:18:43 +0700

    morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const morgan = require('morgan');
 
 const userRoutes = require('./routes/userRoutes');
+
+app.use(morgan('dev'));
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use('/users', userRoutes);
