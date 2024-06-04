139698996820a5731e1bad537c7f015df8c6e9d9
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-05 04:35:51 +0700

    revision addUser routes and add routes for delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,8 @@ const userController = require('../controllers/userControllers');
 
 router.get('/', userController.getAllUsers);
 router.get('/:id_user', userController.getUserById);
-router.post('/registrasi', userController.addUser);
+router.post('/', userController.addUser);
 router.patch('/:id_user', userController.updateUser);
+router.delete('/:id_user', userController.deleteUser);
 
 module.exports = router;
