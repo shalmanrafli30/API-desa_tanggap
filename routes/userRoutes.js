4036468cbf5dcd2fb31569460f9f222a0350345c
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-07 05:15:45 +0700

    sync the laporan with user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
+const laporanController = require('../controllers/laporanControllers');
+const { authenticateToken } = require('../middleware/authMiddleware');
 
 router.get('/', userController.getAllUsers);
 router.get('/id_user=:id_user', userController.getUserById);
 router.post('/', userController.register);
-router.patch('/id_user=:id_user', userController.updateUser);
+router.patch('/id_user=:id_user', authenticateToken, userController.updateUser);
 router.delete('/id_user=:id_user', userController.deleteUser);
 router.post('/login', userController.loginUser);
+router.post('/add-laporan', authenticateToken, laporanController.addLaporan);
 
 module.exports = router;
