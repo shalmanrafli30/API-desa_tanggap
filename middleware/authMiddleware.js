a0de2638245ab4dca44330d9e71efc985aa9880a
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-09 01:32:29 +0700

    add code to check is the code blacklist or not

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,17 @@
 const jwt = require('jsonwebtoken');
+const BlacklistToken = require('../models/blacklistTokenModel');
 
-exports.authenticateToken = (req, res, next) => {
+exports.authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ error: 'Token not provided' });
+    }
+
+    const blacklisted = await BlacklistToken.findOne({ token });
+    if (blacklisted) {
+        return res.status(403).json({ error: 'Token has been blacklisted' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
