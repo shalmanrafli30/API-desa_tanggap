628463bf27359bccb4c41b1df6d29f1229fc363b
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-09 03:56:30 +0700

    get detail laporan routes

--- routes/laporanRoutes.js
+++ routes/laporanRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const laporanController = require('../controllers/laporanControllers');
 
 router.get('/', laporanController.getAllLaporan);
+router.get('/idLaporan=:idLaporan', laporanController.getDetailLaporanById);
 
 module.exports = router;
