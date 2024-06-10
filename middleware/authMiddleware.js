d165c3ca506c30ada427e6d7631057d905bf7476
Shalman Rafli Alva Rizqi <shalmanrafli.asli@gmail.com>
2024-06-10 22:50:08 +0700

    revisi auth

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -22,3 +22,25 @@ exports.authenticateToken = async (req, res, next) => {
         next(); // Proceed to the next middleware or route handler
     });
 };
+
+exports.authenticateTokenAdmin = async (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Token not provided' });
+    }
+
+    const blacklisted = await blacklistModel.isTokenBlacklisted(token);
+    if (blacklisted) {
+        return res.status(403).json({ error: 'Token has been blacklisted' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid token' });
+        }
+        req.id_user = decoded.id_user; // Store user ID for future use
+        next(); // Proceed to the next middleware or route handler
+    });
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userControllers');
 const laporanController = require('../controllers/laporanControllers');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const authenticateToken = require('../middleware/authMiddleware');
 
 router.get('/', userController.getAllUsers);
 router.get('/id_user=:id_user', userController.getUserById);
 router.post('/', userController.register);
-router.patch('/edit-profil', authenticateToken, userController.updateUser);
+router.patch('/edit-profil', authenticateToken.authenticateToken, userController.updateUser);
 router.delete('/id_user=:id_user', userController.deleteUser);
 router.post('/login', userController.loginUser);
-router.post('/logout', authenticateToken, userController.logoutUser);
-router.post('/add-laporan', authenticateToken, laporanController.addLaporan);
-router.get('/laporan-saya', authenticateToken, laporanController.getUserLaporan);
+router.post('/logout', authenticateToken.authenticateToken, userController.logoutUser);
+router.post('/add-laporan', authenticateToken.authenticateToken, laporanController.addLaporan);
+router.get('/laporan-saya', authenticateToken.authenticateToken, laporanController.getUserLaporan);
 
 module.exports = router;
